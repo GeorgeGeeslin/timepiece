afd502d0bfa6d35ef712cea3e03fabe7f1787dd8
George Geeslin <george.geeslin@gmail.com>
2017-09-24 20:51:48 -0500

    added form validation for CreateTaskForm component, and added CSS style for error

--- src/components/CreateTaskForm.js
+++ src/components/CreateTaskForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
+//import { FormGroup, FormControl, HelpBlock } from 'react-bootstrap';
 
 export default class CreatTaskForm extends Component {
 	 static propTypes = {
@@ -12,7 +13,21 @@ export default class CreatTaskForm extends Component {
 		client: ''
 	};
 
+	getValidationState() {
+		const taskLength = this.state.task.length;
+		const projLength = this.state.project.length;
+		if (taskLength > 0 && projLength > 0) {
+			return 'success';
+		} else {
+			return 'error';
+		}
+	}
+
 	onTaskNameChange = (e) => {
+		const taskField = document.getElementById("taskField");
+		if (taskField.className === 'inputError') {
+			taskField.classList.remove('inputError')
+		}
 		const task = e.target.value;
 		this.setState({task: task});
 	};
@@ -29,17 +44,28 @@ export default class CreatTaskForm extends Component {
 
 	addTask = (e) => {
 		if (e) e.preventDefault();
-		this.props.addTask(
-			this.state.task,
-			this.state.project,
-			this.state.client,
-			this.state.timecreated
-		);
-		this.setState({
-			task: '',
-			project: '',
-			client: ''
-		});
+		if (this.state.task.length === 0) {
+			const taskField = document.getElementById("taskField");
+			taskField.classList.add('inputError')
+				this.setState({
+				task: '',
+				project: '',
+				client: ''
+			});
+		} else {
+			this.props.addTask(
+				this.state.task,
+				this.state.project,
+				this.state.client,
+				this.state.timecreated
+			);
+			this.setState({
+				task: '',
+				project: '',
+				client: ''
+			});
+		}
+		console.log("asdf")
 	};
 
 	render() {
@@ -47,24 +73,24 @@ export default class CreatTaskForm extends Component {
 			<div className="creatTaskForm">
 				<form onSubmit={this.addTask}>
 					<input type='submit' value='Add Task' />
-					<input
+					<input id='taskField'
 						type='text'
 						value={this.state.task}
 						onChange={this.onTaskNameChange}
-						placeholder='Task'
+						placeholder='Task (required)'
 					/>
 					<input
 						type='text'
 						value={this.state.project}
 						onChange={this.onProjectNameChange}
 						placeholder='Project'
-					/>		
+					/>	
 					<input
 						type='text'
 						value={this.state.client}
 						onChange={this.onClientNameChange}
 						placeholder='Client'
-					/>								
+					/>						
 				</form>
 			</div>
 		)
