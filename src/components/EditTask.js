93b6b2641ebb00b1c4997f1c61fa1fc8b4fde536
George Geeslin <george.geeslin@gmail.com>
2017-10-12 22:52:42 -0500

    save exit/ cancel buttons now work with time intervals

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -12,6 +12,7 @@ export default class EditTask extends Component {
 		task: this.props.editTask.task,
 		project: this.props.editTask.project,
 		client: this.props.editTask.client,
+		time: this.props.editTask.time,
 		timeintervals: this.props.editTask.timeintervals,
 		invalidTimeIndexs: []
 	};
@@ -36,6 +37,15 @@ export default class EditTask extends Component {
 		return new Date(year,month,day,hours,minutes).getTime();
 	};
 
+	calculateTime = () => {
+		var total = 0;
+		for (var i = 0; i < this.state.timeintervals.length; i++) {
+			total += (this.state.timeintervals[i].stopTime - this.state.timeintervals[i].startTime);
+		}
+
+		return total;
+	};
+
 	onTaskNameChange = (e) => {
 		const editTaskField = document.getElementById('editTaskField');
 		if (editTaskField.className === 'inputError') {
@@ -58,18 +68,20 @@ export default class EditTask extends Component {
 	onChangeStartTime = (index, e) => {
 		const dateTime = e.target.value;
 		const unixTime = this.formatUnixTime(dateTime);
+		var currInterval = {startTime: unixTime, stopTime: this.state.timeintervals[index].stopTime};
 		var timeintervals = [];
 		this.state.timeintervals.map((interval, index) => ( timeintervals.push(interval) ));
-		timeintervals[index].startTime = unixTime;
+		timeintervals[index] = currInterval;
 		this.setState({timeintervals: timeintervals});
 	};
 
 	onChangeStopTime = (index, e) => {
 		const dateTime = e.target.value;
 		const unixTime = this.formatUnixTime(dateTime);
+		var currInterval = {startTime: this.state.timeintervals[index].startTime, stopTime: unixTime};
 		var timeintervals = [];
 		this.state.timeintervals.map((interval, index) => ( timeintervals.push(interval) ));
-		timeintervals[index].stopTime = unixTime;
+		timeintervals[index] = currInterval;
 		this.setState({timeintervals: timeintervals});
 	};
 
@@ -95,10 +107,14 @@ export default class EditTask extends Component {
 			})
 			this.setState({invalidTimeIndexs: invalidTimes})
 		}	else {
+			this.state.time = Math.floor(this.calculateTime() / 1000)
+			console.log(this.state.time);
+
 			this.props.updateTask(
 				this.state.task,
 				this.state.project,
 				this.state.client,
+				this.state.time,
 				this.state.timeintervals,
 				this.props.editTaskIndex
 			)
