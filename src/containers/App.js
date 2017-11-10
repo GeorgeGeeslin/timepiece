c527a48473a2368e43b1bbf93ed8833e5f203533
George Geeslin <george.geeslin@gmail.com>
2017-11-09 22:07:37 -0600

    added App.js as root container to hold the redux store.

---
+++ src/containers/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as TaskActionCreators from '../actions/task';
+import Timepiece from './Timepiece';
+
+class App extends Component {
+	static propTypes = {
+		tasks: PropTypes.array.isRequired
+	};
+
+	//componentWillMount() {
+	//	this.props.uid = 
+	//}
+	
+	render() {
+		const { dispatch, tasks, selectedTaskIndex, showEditScreen, editTaskIndex, lastManualUpdate, username, uid} = this.props;
+		const addTask = bindActionCreators(TaskActionCreators.addTask, dispatch);
+		const selectTask = bindActionCreators(TaskActionCreators.selectTask, dispatch);
+		const finishTask = bindActionCreators(TaskActionCreators.finishTask, dispatch);
+		const deleteTask = bindActionCreators(TaskActionCreators.deleteTask, dispatch);
+		const pauseTask = bindActionCreators(TaskActionCreators.pauseTask, dispatch);
+		const openEdit = bindActionCreators(TaskActionCreators.openEdit, dispatch);
+		const closeEdit = bindActionCreators(TaskActionCreators.closeEdit, dispatch);
+		const updateTask = bindActionCreators(TaskActionCreators.updateTask, dispatch);
+		const resumeTask = bindActionCreators(TaskActionCreators.resumeTask, dispatch);
+
+		return (
+			<div>
+			{ uid === null && <p>User Not Logged in</p>}
+			{ uid !== null &&
+			<Timepiece
+				tasks = {this.props.tasks}
+				selectedTaskIndex = {this.props.selectedTaskIndex}
+				showEditScreen = {this.props.showEditScreen}
+				editTaskIndex = {this.props.editTaskIndex}
+				lastManualUpdate = {this.props.lastManualUpdate}
+				username = {this.props.username}
+				uid = {this.props.uid} 
+				addTask = {addTask}
+				selectTask = {selectTask}
+				finishTask = {finishTask}
+				deleteTask = {deleteTask}
+				pauseTask = {pauseTask}
+				openEdit = {openEdit}
+				closeEdit = {closeEdit}
+				updateTask = {updateTask}
+				resumeTask = {resumeTask} />
+		 }
+		 </div>
+		)
+	}
+}
+
+const mapStateToProps = state => (
+	{
+		tasks: state.tasks,
+		selectedTaskIndex: state.selectedTaskIndex,
+		showEditScreen: state.showEditScreen,
+		editTaskIndex: state.editTaskIndex,
+		lastManualUpdate: state.lastManualUpdate,
+		username: state.username,
+		uid: state.uid
+	}
+);
+
+export default connect(mapStateToProps)(App);
