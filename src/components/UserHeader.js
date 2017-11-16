b7fcadaa9fe3bbe50c4f86d9cc46e65e775cdd8f
George Geeslin <george.geeslin@gmail.com>
2017-11-16 08:12:29 -0600

    improved openeing and closing dropdown. Now toggles when userAvatar is clicked, and also closes when clicking anywhere but the dropdown.

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -11,16 +11,28 @@ export default class UserHeader extends Component {
 		profileDropDown: false
 	}
 
+	componentDidUpdate(prevProps) {
+		console.log(prevProps)
+		if (prevProps.profileDropDown === false && this.state.profileDropDown === true) {
+			document.getElementById("userProfileDropDown").focus();
+		}
+	}
+
 	toggleDropDown = (dropDown) => {
-			this.setState({profileDropDown: !dropDown});	
+		this.setState({profileDropDown: !dropDown});
+	}
+
+	closeDropDown = (dropDown) => {
+		if (dropDown === true) {
+			this.setState({profileDropDown: false});
+		}
 	}
 
 	render () {
 		return (
 			<div>
 			<div className='masthead'>
-				<button tabIndex='0' 
-					onClick={ (e) => this.toggleDropDown(this.state.profileDropDown, e) }className='userAvatar'>
+				<button tabIndex='0' onClick={ () => this.toggleDropDown(this.state.profileDropDown)}className='userAvatar' id='userAvatar'>
 					<img src={this.props.user.photoURL} alt='User Icon' />
 				</button>
 				<span className='displayName'>{this.props.user.displayName}</span>
@@ -28,7 +40,8 @@ export default class UserHeader extends Component {
 				{ this.state.profileDropDown === true &&
 					<Grid>
 						<Row>
-							<Col className='userProfileDropDown' sm={12} md={4} lg={3}>
+							<Col tabIndex='0' className='userProfileDropDown' sm={12} md={4} lg={3} id='userProfileDropDown'
+								onBlur={ () => this.closeDropDown(this.state.profileDropDown)}>
 							<p>{this.props.user.email}</p>
 							<button className='control-buttons' onClick={ () => this.props.attemptSignOut()}>Sign Out</button>
 							</Col>
