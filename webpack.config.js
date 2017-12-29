123af5ffd92a2e13a294512a20d6c0812d25b491
George Geeslin <george.geeslin@gmail.com>
2017-12-29 12:52:59 -0600

    added img-loader and url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ var config = {
 	module: {
 		rules: [
 			{test: /\.(js)$/, use: 'babel-loader'},
-			{test: /\.css$/, use: ['style-loader', 'css-loader']}
+			{test: /\.css$/, use: ['style-loader', 'css-loader']},
+			{test: /\.(jpe?g|png|gif|svg)$/i, use: [{'url-loader?limit=1000', 'img-loader'}]}
 		]
 	},
 	devServer: {
